1a25fe9e4f4c1d1b5e4f3f979a497f341fa57a71
Mahendra pathak <mahendrapathak87@gmail.com>
2020-05-11 00:39:15 -0400

    Simple basic graphql module

---
+++ Simple/GraphqlDemo/Model/Resolver/ModeldataQueryResolver.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Simple\GraphqlDemo\Model\Resolver;
+
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Config\Element\Field;
+
+class ModeldataQueryResolver implements ResolverInterface{
+    
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value=null, array $args=null){
+        return ['string1','string2'];
+    }
+}
---
+++ Simple/GraphqlDemo/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* @category 
+*
+* @author mahendra
+* @copyright Copyright 
+* @license 
+*/
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Simple_GraphqlDemo',
+    __DIR__
+);
