7cbf2750f0c0bc74f48037a1cd9e1970fab99733
Mahendra pathak <mahendrapathak87@gmail.com>
2020-06-03 23:59:18 -0400

    Added changes data-mage-init example

---
+++ Simple/JavascriptIntro/Controller/Index/Jstwo.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Copyright © JavascriptIntro, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Simple\JavascriptIntro\Controller\Index;
+
+class Jstwo extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		return $this->_pageFactory->create();
+	}
+}
---
+++ Simple/JavascriptIntro/view/frontend/web/js/jstwo.js
@@ -0,0 +1,8 @@
+define(function(){
+    'use strict';
+    
+    return function(config,element){
+        console.log('hello world',config,element);
+    }
+    
+});
